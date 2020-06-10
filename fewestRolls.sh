a62d8a59f9f89e624393fa3692876fb9587b856f
Andrew Nathenson <ColorfulPockets01@gmail.com>
2020-06-09 17:54:54 -0700

    added Kit's increment idea

--- fewestRolls.sh
+++ fewestRolls.sh
@@ -4,18 +4,32 @@ moves=( 'R' 'L' 'U' 'D' 'B' 'F' )
 isDone=0
 setNumber=1
 moveCount=0
+pocketSize=7
+incrementPocket=0
 printf "Scramble: "
 read
 while [ $isDone -ne 1 ]; do
 	count=0
 	moveSet=()
 	echo -n ""
-	until [ $count -gt 11 ]; do
+	until [ $count -gt $pocketSize ]; do
 		index=$[$RANDOM % ${#moves[@]}]
 		moveType=${moves[${index}]}
 		moveSet+=($moveType)
 		let count=count+1
 	done
+
+	if [ $pocketSize -lt 12 ]; then
+		if [ $incrementPocket -eq 0 ]; then
+			let incrementPocket=1
+		else	
+			let incrementPocket=0
+			let pocketSize=pocketSize+1
+		fi
+	else
+		let pocketSize=pocketSize+1
+	fi
+
 	IFS=$'\n' sorted=($(sort <<<"${moveSet[*]}"))
 	unset IFS
 	printf "\n%d: " $setNumber
