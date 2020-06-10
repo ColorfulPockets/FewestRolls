b1d611ce17b7be58b5ca6185d8e4cb1b6ad78959
Andrew Nathenson <ColorfulPockets01@gmail.com>
2020-06-10 01:36:27 -0700

    added counter for number of moves

--- Buffer/bufferCube.py
+++ Buffer/bufferCube.py
@@ -15,15 +15,23 @@ def incrementBuffer(buffer):
         buffer[i] = buffer[i+1]
     buffer[BUFFERSIZE - 1] = moves[random.randint(0,5)]
 
+def exitSequence(count):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(count)
+    quit()
+
 def main():
     done = False
+    count = 1
     buffer = generateBuffer()
     while not done:
         print(buffer)
         x = input()
         if x == 'done' or x == "Done" or x == "DONE":
             done = True
+            exitSequence(count)
         incrementBuffer(buffer)
+        count += 1
 
 if __name__ == "__main__":
     main()
